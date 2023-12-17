d1caf91b87bf975d2226c22ae5df7815f4bd4ecd
Vu <nguyentuanvu9x@gmail.com>
2023-12-17 09:27:34 +0300

    Updating: Generics 1st and 3rd method

--- src/main/java/com/epam/mjc/generics/Generics.java
+++ src/main/java/com/epam/mjc/generics/Generics.java
@@ -6,10 +6,10 @@ import java.util.List;
 public class Generics {
 
     //TODO: Refactor Method-1
-    public <T> List<List<T>> boxingMethod(T name) {
-        List<T> firstList = new ArrayList<>();
+    public List<List<String>> boxingMethod(String name) {
+        List<String> firstList = new ArrayList<>();
         firstList.add(name);
-        List<List<T>> secondList = new ArrayList<>();
+        List<List<String>> secondList = new ArrayList<>();
         secondList.add(firstList);
         return secondList;
     }
@@ -20,7 +20,7 @@ public class Generics {
     }
 
     //TODO: Refactor Method-3
-    public<T> void cloneMethod(List<T> consumer, List<? extends T> producer) {
+    public<T> void cloneMethod(List<? super T> consumer, List<? extends T> producer) {
         consumer.addAll(producer);
     }
 
